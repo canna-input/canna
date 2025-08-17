33e4caebfd74a440515155f1b5e84f3a271df896
AIDA Shinra <shinra@j10n.org>
2025-08-18 00:05:53 +0900

    [Merged] from the SFJP trunk on 2004-11-21: * New CRC calculator was buggy. * lib/RKindep/mkcrctab.c: create correct CRC table * lib/RKindep/crctab.c: newly generated [Merged] from the SFJP trunk on 2004-05-03: * Reimplement CRC calculator to avoid old BSD license. * lib/RKindep/cksum.c: reimplemented everything from FreeBSD * lib/RKindep/crctab.c: added * lib/RKindep/mkcrctab.c: added (but not compiled) * lib/RKindep/Imakefile: mkcrctab rule

---
+++ lib/RKindep/mkcrctab.c
@@ -0,0 +1,32 @@
+#include "cannaconf.h"
+#include "ccompat.h"
+#include <stdio.h>
+
+RCSID("$Id: mkcrctab.c,v 1.2 2004/11/21 06:37:40 aida_s Exp $");
+
+int
+main(argc, argv)
+int argc;
+char *argv[];
+{
+  canna_uint32_t i, r;
+  unsigned int j;
+  static canna_uint32_t table[256];
+
+  for (i = 0; i < 256; ++i) {
+    r = i << 24;
+    for (j = 0; j < 8; ++j) {
+      if (r & (1UL << 31))
+	r = (r << 1) ^ 0x04c11db7UL;
+      else
+	r <<= 1;
+    }
+    table[i] = r;
+  }
+  for (j = 0; j < 64; ++j)
+    printf("  0x%08x, 0x%08x, 0x%08x, 0x%08x,\n",
+	table[j*4], table[j*4+1], table[j*4+2], table[j*4+3]);
+  return 0;
+}
+
+/* vim: set sw=2: */
